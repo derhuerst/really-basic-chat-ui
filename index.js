ecda7d0fc31f7c55b46b02ee43cbf2e37ea5e272
Jannis R <mail@jannisr.de>
2018-03-12 01:15:57 +0100

    input: handle line breaks :bug:, cache color hashes globally

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ const termSize = require('window-size').get
 const ms = require('ms')
 const wrap = require('prompt-skeleton')
 const cli = require('cli-styles')
+const stringWidth = require('string-width')
+const runes = require('runes')
 
-const colorHash = s => '#' + md5(s).slice(0, 6)
+const colorHashes = Object.create(null)
+const colorHash = (user) => {
+	if (user in colorHashes) return colorHashes[user]
+	const hash = '#' + md5(user).slice(0, 6)
+	colorHashes[user] = hash
+	return hash
+}
+
+const _cleanStr = str => stripAnsi(str).replace(/(\n|\r|\r\n)/g, ' ↵ ')
+
+const cleanedStrs = Object.create(null)
+const cleanStr = (str) => {
+	if (str in cleanedStrs) return cleanedStrs[str]
+	const cleanedStr = _cleanStr(str)
+	cleanedStrs[str] = cleanedStr
+	return cleanedStr
+}
 
 const UI = {
 	abort: function () {
@@ -17,14 +35,14 @@ const UI = {
 	},
 	submit: function () {
 		if (!this.input) return
-		this.send(stripAnsi(this.input))
+		this.send(this.input)
 		this.input = ''
 		this.render()
 		clearInterval(this.renderInterval)
 	},
 
 	_: function (key) {
-		this.input += key
+		this.input += _cleanStr(key)
 		this.render()
 	},
 	delete: function () {
@@ -36,19 +54,16 @@ const UI = {
 	render: function (first) {
 		const {width, height} = termSize()
 
-		const colorOf = Object.create(null)
 		let out = ''
 		if (this.messages.length > 0) {
 			const messages = this.messages.slice(-height + 2)
 			const now = Date.now()
 
 			for (let msg of messages) {
-				if (!colorOf[msg.from]) colorOf[msg.from] = colorHash(msg.from)
-
 				if (msg.sending) out += '⌛ '
 				out += [
 					chalk.gray(ms(now - msg.when)),
-					chalk.hex(colorOf[msg.from])(msg.from),
+					chalk.hex(colorHash(msg.from))(msg.from),
 					msg.content
 				].join(' ') + '\n'
 			}
